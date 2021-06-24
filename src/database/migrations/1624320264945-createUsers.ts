59a10787e7224c1aff0a7ce8b3c894476ca0bd12
Fábio Souza <74217251+Fabio-SS@users.noreply.github.com>
2021-06-24 13:04:39 -0300

    Update Aula 4
    Aula 4 completa

--- src/database/migrations/1624320264945-createUsers.ts
+++ src/database/migrations/1624320264945-createUsers.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class createUsers1624320264945 implements MigrationInterface {
+export class CreateUsers1624320264945 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
